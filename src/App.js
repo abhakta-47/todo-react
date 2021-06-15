19990ed9bab7e8af1c12e6ae53f496082645394b
AbDev <ab.inpathtoadev@gmail.com>
2021-06-15 22:08:04 +0530

    feature : local storage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 // import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchForm from './components/SearchForm'
 import ToDoList from './components/ToDoList'
 import AddForm from './components/AddForm'
 
 function App() {
 
-  // const [taskList,setTaskList] = useState([])
-
   const [todoList,setTodoList]=useState([]);
+
+  useEffect(()=>{
+    const rawData=localStorage.getItem("todoList");
+    const savedTodoList = JSON.parse(rawData);
+    // console.log('called 2', savedTodoList);
+    setTodoList(savedTodoList);
+  },[])
   
+  useEffect(() => {
+    localStorage.setItem("todoList", JSON.stringify(todoList));
+    // console.log('called 1');
+  },[todoList] )
+
   return (
     <div className="app">
       <header>
